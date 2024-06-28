9476c7140018e03ddcdf69a9c4136693300dea0d
06171002 <guri8376@gmail.com>
2024-06-28 12:58:49 +0900

    0628

---
+++ java_work/Ex02/src/aa/Ex07.java
@@ -0,0 +1,18 @@
+package aa;
+
+public class Ex07 {
+	
+	public static void main(String[] args) {
+		
+		final int MAX_SIZE = 100;
+		System.out.println(MAX_SIZE);
+		
+		float result = (float) (3.3 + 4.5);
+		System.out.println(result);
+		
+		System.out.printf("정수는 %d 실수는 %f",10,20.33);
+		System.out.println();
+		System.out.println(31474836147589032l);
+	}
+
+}
---
+++ java_work/Ex02/src/aa/Ex08.java
@@ -0,0 +1,10 @@
+package aa;
+
+public class Ex08 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("\"안녕하세요\"\n\"이상진입니다.\"\n\t개발공부는.....");
+	}
+
+}
