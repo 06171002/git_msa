1f3acc12aa01a6dc70b2485b18812c465e3f78fc
06171002 <guri8376@gmail.com>
2024-07-11 18:10:10 +0900

    0711

--- 0711/src/Problem4.java
+++ 0711/src/Problem4.java
@@ -2,6 +2,31 @@ public class Problem4 {
 
     public static void main(String[] args) {
 
+        int[] arr = {42, 100, 34, 2};
 
+        int max_idx = 0;
+
+
+        for (int i = 3; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                if (arr[max_idx] < arr[j]) {
+                   max_idx = j;
+
+
+                }
+            }
+            swap(arr, i, max_idx);
+            System.out.println(arr[0]+" "+arr[1]+" "+arr[2]+" "+arr[3]);
+
+        }
+
+
+    }
+
+    private static void swap(int[] arr, int i, int max_idx) {
+        int tmp;
+        tmp = arr[i];
+        arr[i] = arr[max_idx];
+        arr[max_idx] = tmp;
     }
 }
