ca28c90ddbd0706859effc5f0246d2f497fe732b
06171002 <guri8376@gmail.com>
2024-07-18 18:07:07 +0900

    0718

--- Mavens/src/main/java/org/example/Problem3.java
+++ Mavens/src/main/java/org/example/Problem3.java
@@ -6,11 +6,11 @@ public class Problem3 {
 
     public static void main(String[] args) {
 
-        int[][] arr ={{1,2,3},{4,5,6},{7,8,9}};
+        int[][] arr ={{1,2,3,4,5,6,7},{4,5,6,7,8,9,10},{7,8,9,10,11,12,13},{1,2,3,4,5,6,7},{6,7,8,9,10,11,12},{1,2,3,4,5,6,7}};
         int[][] arr1 =new int[arr.length][arr.length];
 
         for (int i = 0; i < arr.length; i++) {
-            arr1[(i+1)%3] = arr[i];
+            arr1[(i+1)%6] = arr[i];
 
         }
         System.out.print(Arrays.deepToString(arr1));
