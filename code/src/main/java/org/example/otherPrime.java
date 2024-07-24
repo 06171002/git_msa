e10ce488b23876c626eec37742feabe9ee0ebb8e
06171002 <guri8376@gmail.com>
2024-07-24 09:23:44 +0900

    0724

---
+++ code/src/main/java/org/example/otherPrime.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class otherPrime {
+    public static void main(String[] args) {
+        boolean[] arr = new boolean[100];
+        Arrays.fill(arr,true);
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 2; i < arr.length ; i++) {
+            if (arr[i] == true) {
+                count++;
+                sum += i;
+                System.out.print(i + " ");
+                for (int j = 2*i; j < arr.length; j+=i) {
+                    arr[j] = false;
+                }
+            }
+
+        }
+        System.out.println();
+        System.out.println(count);
+        System.out.println(sum);
+
+    }
+
+}
