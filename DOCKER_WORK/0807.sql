c95e46af67a2cbb8b389f433cf6c92d1672818af
06171002 <guri8376@gmail.com>
2024-08-09 18:12:26 +0900

    0809

--- DOCKER_WORK/0807.sql
+++ DOCKER_WORK/0807.sql
@@ -183,6 +183,10 @@ from employees e inner join jobs j on e.job_id = j.job_id
 where j.job_title like '%manager%'
 group by e.manager_id;
 
+select e1.employee_id, min(e1.salary)
+from employees e1, employees e2
+where e1.manager_id = e2.employee_id and e2.employee_id = 100;
+
 select e.first_name, e.job_id
 from employees e inner join jobs j on e.job_id = j.job_id
 where j.job_title like '%manager%';
@@ -266,4 +270,57 @@ from locations
 where length(street_address) in (select min(length(street_address))
 from locations);
 
+-- 사원번호 이름 급여 업무 입사일 상사 사원번호 이름 성과이름 연결alter
+select concat(e1.last_name,e1.first_name), e1.employee_id, e2.employee_id '상사 사원번호'
+from employees e1, employees e2
+where e1.manager_id = e2.employee_id;
+
+select concat(last_name,first_name)
+from employees;
+
+-- 연봉 보너스 100 급여 급여 보너스 100
+select round(salary/12,0) 
+from employees;
+
+select concat(last_name,first_name) 'name', job_id, salary, salary + 100 ' Increased Salary', salary*12 + 100 'Increased Ann_Salary'
+from employees;
+-- 이름: 1 year salary = 연봉 
+select concat(last_name, '   ','Year Salary=',salary)
+from employees;
+-- 부서별로 담당 업무 한번씩
+select job_id, department_id
+from employees
+group by department_id, job_id
+order by department_id;
+-- 급여 7000 10000 성과이름 급여가 작은 순서대로
+select concat(last_name,first_name), salary
+from employees
+where salary between 7000 and 10000
+order by salary;
+-- 성 중에 e 및 o 포함된 사원 e and o name
+select concat(last_name,first_name) 'e and o name'
+from employees
+where substr(last_name,1) like ('%e%');
+
+select employee_id, concat(last_name,first_name) 'e and o name'
+from employees
+where last_name like '%e%' or last_name like '%o%';
+-- 1995년 5월 20일부터 1996년 5월 20일 사이에 고용된 사원 이름 사원번호 고용일자 입사일 빠른순서
+select employee_id, concat(last_name,first_name), hire_date
+from employees
+where hire_date between '1995-05-20' and '1996-05-20'
+order by hire_date; 
+-- 수당을 받는 모든 사원 성과 이름 급여 업무 수당률 급여 큰 순서 수당률 큰 순서
+select concat(last_name,first_name) 'name', salary, commission_pct
+from employees
+where commission_pct not in (0)
+order by commission_pct;
+-- 60번 it부서 사원의 급여 12.3% 인상하여 정수만 표시 사번, 이름 성 인상된 급여 
+select concat(last_name,first_name) 'name', salary, round(salary*1.123,0) '12.3% 인상된 급여' , round(salary*1.123 - salary,0) '인상된 급여 - 이전 급여'
+from employees; 
+
+--  이름이 's'로 끝나는 각 사원의 이름과 업무를 다음의 예시와 같이 출력하세요. (예시 : Sigal Tobias is a PU_CLERK) 출력 시 성과 이름은 첫 글자를 대문자로, 업무는 모두 대문자로 출력하고 머리글은 Employee JOBs로 표시하세요. (18행)
+select concat(last_name, first_name, ' is a ',job_id) 'Employee JOBs'
+from employees
+where first_name like '%s';
 
