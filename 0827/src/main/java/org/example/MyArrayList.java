5a42649ccc5ac0eab43508dc9e6dd4b93a50c27b
06171002 <guri8376@gmail.com>
2024-08-27 18:15:31 +0900

    0827

--- 0827/src/main/java/org/example/MyArrayList.java
+++ 0827/src/main/java/org/example/MyArrayList.java
@@ -18,12 +18,38 @@ public class MyArrayList {
     }
 
     public void add(Object o) {
+        if (size == objects.length){
+            grow();
+        }
         this.objects[size] = o;
         size++;
     }
 
+    private void grow() {
+        int oldValue = objects.length;
+        int newValue = oldValue*2;
+        objects = Arrays.copyOf(objects,newValue);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int indexOf(Object o) {
+        for (int i = 0; i < objects.length; i++) {
+            if (objects[i] == o) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Object get(int n) {
+        return objects[n];
+    }
+
     @Override
     public String toString() {
-        return Arrays.toString(Arrays.copyOfRange(objects,0,size));
+        return Arrays.toString(Arrays.copyOf(objects,size));
     }
 }
--- 0827/src/main/java/org/example/MyArrayListTest.java
+++ 0827/src/main/java/org/example/MyArrayListTest.java
@@ -9,8 +9,18 @@ public class MyArrayListTest {
 
         MyArrayList myArrayList = new MyArrayList();
         myArrayList.add(10);
+        myArrayList.add(20);
+        myArrayList.add(30);
+        myArrayList.add(40);
+        myArrayList.add(50);
+
+        System.out.println(myArrayList.indexOf(30));
+        System.out.println(myArrayList.get(0));
 
         System.out.println(myArrayList);
+
+        myArrayList.add(60);
+        System.out.println(myArrayList.size());
 
 
     }
